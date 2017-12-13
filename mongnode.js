5f97fe608e44d98461a49f7819d0213cc815738b
KAN <kabirajitnagral@gmail.com>
2017-12-12 23:07:35 -0500

    Added function to create database

--- mongnode.js
+++ mongnode.js
@@ -1,1 +1,14 @@
 var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
+var url = "mongodb://localhost:27017/mydb";
+
+//Creating Database
+function createDatabase ()
+{
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+  });
+}
